6605d711d010d1444c6de6f753dfb1dba45a8732
Obez99 <giuliogx@live.it>
2021-11-26 14:36:18 +0100

    Prints test paragraph in html

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$myParagraph = "Questo è un paragrafo di prova per l'esercizio di oggi.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php Badwords</title>
+</head>
+
+<body>
+  <p><?php echo $myParagraph ?></p>
+</body>
+
+</html>
